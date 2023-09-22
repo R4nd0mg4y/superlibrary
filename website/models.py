c9bd3e991eeb95373bd45f898787e0eea5309be2
R4nd0mg4y <tuandoviet2002@gmail.com>
2023-09-22 08:43:14 +0700

    lõi be

--- website/models.py
+++ website/models.py
@@ -31,4 +31,3 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
     first_name = db.Column(db.String(128))
-    notes = db.relationship('Note')
